6cb4514db38d6a21bd1631dd4e15fdb45daa97e0
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-31 21:52:41 +0100

    feat(database): Add singleton database connection pattern file

---
+++ App/Config/Database.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . 'Config.php';
+class Database
+{
+    private static ?Database $instance = null;
+    private PDO $connexion;
+    private function __construct()
+    {
+        $this->connexion = new PDO('mysql:host=' . HOST . 
+                                    'dbname=' . DB_NAME , USER, PASSWORD);
+    }
+
+    public static function getInstance() : Database {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() : PDO{
+        return $this->connexion;
+    }
+    
+}
